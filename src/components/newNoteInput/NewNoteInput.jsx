9fcf947008bbbde30487021ce2073943f8f07293
Apurva Sawant <apurvasawant45@gmail.com>
2022-04-03 14:02:37 +0530

    fix : sidebar issue

--- src/components/newNoteInput/NewNoteInput.jsx
+++ src/components/newNoteInput/NewNoteInput.jsx
@@ -64,101 +64,99 @@ const NewNoteInput = ({ user, tagsList }) => {
   };
 
   return (
-    <div className="home-container">
-      <div className="note-editor" style={{ backgroundColor: note.noteColor }}>
-        <div className="note-header flex-row-center">
-          <div
-            className="title-container"
-            onClick={() => {
-              setShowEditor(true);
-            }}
-          >
-            <input
-              type="text"
-              className="note-title"
-              placeholder="Title"
-              value={note.title}
-              onChange={(e) =>
-                setNote({
-                  ...note,
-                  title: e.target.value,
-                })
-              }
-            />
-          </div>
-
-          <span
-            className="icon mr-1"
-            onClick={() => setNote({ ...note, isPinned: !note.isPinned })}
-          >
-            {note.isPinned ? <PinFillIcon /> : <PinIcon />}
-          </span>
-
-          {showEditor && (
-            <>
-              <span
-                className="primary-text note-add pr-1"
-                onClick={addNoteHandler}
-              >
-                ADD
-              </span>
-              <span className="editor-close-icon">
-                <CloseIcon
-                  size="1.5rem"
-                  onClick={() => {
-                    setNote(initialNoteState);
-                    setValue("");
-                    setShowEditor(false);
-                  }}
-                />
-              </span>
-            </>
-          )}
+    <div className="note-editor" style={{ backgroundColor: note.noteColor }}>
+      <div className="note-header flex-row-center">
+        <div
+          className="title-container"
+          onClick={() => {
+            setShowEditor(true);
+          }}
+        >
+          <input
+            type="text"
+            className="note-title"
+            placeholder="Title"
+            value={note.title}
+            onChange={(e) =>
+              setNote({
+                ...note,
+                title: e.target.value,
+              })
+            }
+          />
         </div>
 
+        <span
+          className="icon mr-1"
+          onClick={() => setNote({ ...note, isPinned: !note.isPinned })}
+        >
+          {note.isPinned ? <PinFillIcon /> : <PinIcon />}
+        </span>
+
         {showEditor && (
-          <span>
-            <ReactQuill
-              theme="snow"
-              value={value}
-              onChange={setValue}
-              placeholder={"Write something awesome..."}
-              modules={modules}
-              formats={formats}
-              ref={editorRef}
-            />
-
-            <span className="editor-footer pr-1 flex-row">
-              <EditorToolbar />
-              <span>
-                <select
-                  className="priority-dropdown mr-1"
-                  onChange={(e) => priorityHandler(e.target.value)}
-                >
-                  <option value={null} hidden="">
-                    Priority
-                  </option>
-                  <option value="0">Low</option>
-                  <option value="1">Medium</option>
-                  <option value="2">High</option>
-                </select>
-
-                <TagIcon onClick={() => setDropdown(true)} className="mr-1" />
-                {dropdown && (
-                  <DropDown
-                    list={tagsList}
-                    type="checkbox"
-                    note={note}
-                    setNote={setNote}
-                    setDropdown={setDropdown}
-                  />
-                )}
-                <ColorPalette user={user} note={note} setNote={setNote} />
-              </span>
+          <>
+            <span
+              className="primary-text note-add pr-1"
+              onClick={addNoteHandler}
+            >
+              ADD
             </span>
-          </span>
+            <span className="editor-close-icon">
+              <CloseIcon
+                size="1.5rem"
+                onClick={() => {
+                  setNote(initialNoteState);
+                  setValue("");
+                  setShowEditor(false);
+                }}
+              />
+            </span>
+          </>
         )}
       </div>
+
+      {showEditor && (
+        <span>
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            placeholder={"Write something awesome..."}
+            modules={modules}
+            formats={formats}
+            ref={editorRef}
+          />
+
+          <span className="editor-footer pr-1 flex-row">
+            <EditorToolbar />
+            <span>
+              <select
+                className="priority-dropdown mr-1"
+                onChange={(e) => priorityHandler(e.target.value)}
+              >
+                <option value={null} hidden="">
+                  Priority
+                </option>
+                <option value="0">Low</option>
+                <option value="1">Medium</option>
+                <option value="2">High</option>
+              </select>
+
+              <TagIcon onClick={() => setDropdown(true)} className="mr-1" />
+              {dropdown && (
+                <DropDown
+                  list={tagsList}
+                  type="checkbox"
+                  note={note}
+                  setNote={setNote}
+                  setDropdown={setDropdown}
+                />
+              )}
+              <ColorPalette user={user} note={note} setNote={setNote} />
+            </span>
+          </span>
+        </span>
+      )}
     </div>
   );
 };
